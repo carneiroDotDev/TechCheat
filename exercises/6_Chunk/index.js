93880bca03f2efc2c272daf0611db6603b1cf041
Luiz Carneiro <luuizpaulo@gmail.com>
2019-10-25 13:13:29 +0200

    Added 5_FizzBuzz and 6_Chunk

---
+++ exercises/6_Chunk/index.js
@@ -0,0 +1,53 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+/*
+function chunk(array, size) {
+     const store_arr = []
+     let temp_arr = []
+    for (let el of array){
+        if(temp_arr.length < size){
+            temp_arr.push(el)
+        } else {
+          store_arr.push(temp_arr)
+          temp_arr = [el]
+        }
+    }
+    if (temp_arr.length){
+        store_arr.push(temp_arr)
+    }
+    return store_arr
+}
+*/
+
+/*
+function chunk(array, size){
+    const chunked = []
+    for (let element of array){
+        const last = chunked[chunked.length - 1]
+        if (!last || last.length === size){
+            chunked.push([element])
+        } else {
+            last.push(element)
+        }
+    }
+    return chunked
+}
+*/
+
+function chunk(array, size){
+    const chunked = []
+    for (let i=0; i<array.length; i+= size){
+            chunked.push( array.slice(i, i + size) )
+    }
+    return chunked
+}
+
+module.exports = chunk;
