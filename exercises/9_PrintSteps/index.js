97c5a9ca9bc680ea96bd1c37fb37d1f9317e3470
Luiz Carneiro <luuizpaulo@gmail.com>
2019-10-27 14:39:57 +0100

    Added recursive solution for 9_PrintSteps

--- exercises/9_PrintSteps/index.js
+++ exercises/9_PrintSteps/index.js
@@ -17,6 +17,69 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+/*
+function strConst(n){
+    let str=''
+    for (let i = 0; i<n; i++){
+        str += "#"
+    }
+    return str
+}
+
+function steps(n) {
+    for (let i = 1; i<=n; i++){
+         let str = ''
+        str = strConst(i)
+        if(str.length < n){    
+        for (let i = str.length; i<n; i++){
+            str += " "
+        }}
+        console.log(str)
+    }
+    return
+}
+
+function steps(n){
+    if (!n){return}
+     for (let i=1; i<=n; i++){
+        let str=""
+         for (let j=0; j<i; j++){
+             str+="#"
+         }
+         if(str.length < n){
+            for (let z = str.length; z<n; z++){
+                 str += " "
+        }}
+         console.log(str)
+         }
+     }
+
+function steps(n){
+     for (let i=0; i<n; i++){
+        let str=""
+         for (let j=0; j < n; j++){
+             if(j <= i){
+                 str+="#"
+             } else {
+                 str+=" "
+             }         
+         }
+        console.log(str)
+     }
+    return
+}
+*/
+
+function steps(n, row = 0, stair = ''){
+    if(row === n){return;}
+
+    if(stair.length === n){
+       console.log(stair);
+       return steps(n, row + 1);
+    }
+
+    let add = stair.length <= row ? '#' : ' ';
+    steps(n, row, stair + add);
+}
 
 module.exports = steps;
