e09c2e9e2e47e7e14384565004311de53ce988bc
Luiz Carneiro <luuizpaulo@gmail.com>
2019-10-28 21:03:41 +0100

    Added solution for 10_Pyramid

--- exercises/10_Pyramid/index.js
+++ exercises/10_Pyramid/index.js
@@ -14,6 +14,38 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+/*
+function pyramid(n) {
+     for(let i=0; i<n; i++){
+         let str = ""
+         for(let j=0; j < (2*n -1); j++){
+             if( (j >= Math.floor( (2*n -1)/2 )-i) && 
+                 (j <= Math.floor( (2*n -1)/2 )+i)){
+                 str +="#"
+             } else {
+                 str+=' '
+             }
+         }
+         console.log(str)
+     }
+} */
+
+function pyramid(n, row=0, str=""){
+    if(row === n){return}
+
+    if(str.length === (2*n-1) ){
+         row+=1
+         console.log(str)
+         return pyramid(n, row)
+    }
+    
+    if( (str.length >= Math.floor( (2*n -1)/2 )-row) && 
+        (str.length <= Math.floor( (2*n -1)/2 )+row)){
+        str +="#"
+    } else {
+        str+=' '
+    }
+    pyramid(n, row, str)
+}
 
 module.exports = pyramid;
