f53a9db46ee832cecd018b515ce5de2ba5aa2af5
Luiz Carneiro <luuizpaulo@gmail.com>
2019-11-10 16:06:16 +0100

    TwoStacksForQueue added

--- exercises/17_TwoStacksForQueue/index.js
+++ exercises/17_TwoStacksForQueue/index.js
@@ -14,6 +14,42 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor(){
+        this.stack1 = new Stack()
+        this.stack2 = new Stack()
+    }
+
+    add(resource){
+        this.stack1.push(resource)
+    }
+
+    remove(){
+        while (this.stack1.peek()){
+            this.stack2.push(this.stack1.pop())
+        }
+        const removedItem = this.stack2.pop()
+
+        while (this.stack2.peek()){
+            this.stack1.push(this.stack2.pop())
+        }
+
+        return removedItem
+
+    }
+
+    peek(){
+        while (this.stack1.peek()){
+            this.stack2.push(this.stack1.pop())
+        }
+        const pickedItem = this.stack2.peek()
+
+        while (this.stack2.peek()){
+            this.stack1.push(this.stack2.pop())
+        }
+
+        return pickedItem
+    }
+}
 
 module.exports = Queue;
