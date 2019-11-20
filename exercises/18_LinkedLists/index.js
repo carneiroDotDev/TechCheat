6dc889d2e4403fccea7e8be35b14a4450115229c
Luiz Carneiro <luuizpaulo@gmail.com>
2019-11-20 20:44:42 +0100

    Reusing methods

--- exercises/18_LinkedLists/index.js
+++ exercises/18_LinkedLists/index.js
@@ -14,7 +14,8 @@ class LinkedList {
         this.head = null
     }
     insertFirst(value) {
-        this.head = new Node(value, this.head)
+        // this.head = new Node(value, this.head)
+        this.insertAt(value, 0)
     }
     size() {
         let counter = 0
@@ -28,18 +29,20 @@ class LinkedList {
     }
 
     getFirst() {
-        return this.head
+        // return this.head
+        return this.getAt(0)
     }
 
     getLast() {
-        if (!this.head) {
-            return null
-        }
-        let node = this.head
-        while (node.next) {
-            node = node.next
-        }
-        return node
+        // if (!this.head) {
+        //     return null
+        // }
+        // let node = this.head
+        // while (node.next) {
+        //     node = node.next
+        // }
+        // return node
+        return this.getAt(this.size() - 1)
     }
 
     clear() {
@@ -47,27 +50,29 @@ class LinkedList {
     }
 
     removeFirst() {
-        if (this.head) {
-            this.head = this.head.next
-        }
+        // if (this.head) {
+        //     this.head = this.head.next
+        // }
+        this.removeAt(0)
     }
 
     removeLast() {
-        if (!this.head) {
-            return
-        }
-        if (!this.head.next) {
-            this.head = null
-            return
-        }
-        let node = this.head
-        let previousNode = node
-        let nextNode = node.next
-        while (nextNode.next) {
-            previousNode = nextNode
-            nextNode = nextNode.next
-        }
-        previousNode.next = null
+        this.removeAt(this.size() - 1)
+        // if (!this.head) {
+        //     return
+        // }
+        // if (!this.head.next) {
+        //     this.head = null
+        //     return
+        // }
+        // let node = this.head
+        // let previousNode = node
+        // let nextNode = node.next
+        // while (nextNode.next) {
+        //     previousNode = nextNode
+        //     nextNode = nextNode.next
+        // }
+        // previousNode.next = null
     }
 
     insertLast(value) {
@@ -103,22 +108,22 @@ class LinkedList {
         if (!this.head) {
             return null
         }
-        if (index === 0 && this.head.next) {
+
+        if (index === 0) {
             this.head = this.head.next
             return
         }
-        const previousNode = this.getAt(index - 1)
-        const eliminationNode = this.getAt(index)
-        const nextNode = this.getAt(index + 1)
 
-        if (!eliminationNode) {
-            return null
+        if (!this.getAt(index)) {
+            return
         }
-        if (!nextNode && previousNode) {
+
+        const previousNode = this.getAt(index - 1)
+        if (!previousNode.next) {
             previousNode.next = null
             return
         }
-        previousNode.next = nextNode
+        previousNode.next = previousNode.next.next
     }
 
     insertAt(value, index) {
