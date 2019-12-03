237e63806191cb1e1b5ca40def399ceb2de610b1
Luiz Carneiro <luuizpaulo@gmail.com>
2019-12-03 22:33:22 +0100

    added depth-first-search

--- exercises/22_Tree/index.js
+++ exercises/22_Tree/index.js
@@ -11,32 +11,41 @@
 // function that gets called with each element in the tree
 
 class Node {
-    constructor(data){
+    constructor(data) {
         this.data = data;
         this.children = []
     }
 
-    add(data){
+    add(data) {
         this.children.push(new Node(data))
     }
 
-    remove(data){
-        this.children = this.children.filter( node => node.data !== data)
+    remove(data) {
+        this.children = this.children.filter(node => node.data !== data)
     }
 }
 
 class Tree {
-    constructor(){
-        this.root = null 
+    constructor() {
+        this.root = null
     }
 
-    traverseBF(fn){
-            const arr = [this.root]
-            while(arr.length){
-                const node = arr.shift()
-                arr.push(...node.children)
-                fn(node)
-            }
+    traverseBF(fn) {
+        const arr = [this.root]
+        while (arr.length) {
+            const node = arr.shift()
+            arr.push(...node.children)
+            fn(node)
+        }
+    }
+
+    traverseDF(fn) {
+        const arr = [this.root]
+        while (arr.length) {
+            const node = arr.shift()
+            arr.unshift(...node.children)
+            fn(node)
+        }
     }
 }
 
