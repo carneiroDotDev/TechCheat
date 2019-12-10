07ca1ea23189184a6cc0897c8a44ce79512d0a61
Luiz Carneiro <luiz.carneiro@sport1.de>
2019-12-10 17:36:04 +0100

    Added Bubblesort solution

--- exercises/27_BubbleSort/index.js
+++ exercises/27_BubbleSort/index.js
@@ -11,6 +11,18 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+function BubbleSort(arr){
+    let less = 0
+    for (let i = 0; i<arr.length; i++){
+        for (let j = 0; j<(arr.length-1); j++){
+            if(arr[j+1] < arr[j]){
+                less = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = less
+            }
+        }
+    }
+    return arr
+}
 
-module.exports = Node;
+module.exports = BubbleSort;
