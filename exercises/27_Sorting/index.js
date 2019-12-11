ac4f60837262867de8dedcadfb186f6edab19060
Luiz Carneiro <luuizpaulo@gmail.com>
2019-12-11 12:56:17 +0100

    Bubblesort added

--- exercises/27_Sorting/index.js
+++ exercises/27_Sorting/index.js
@@ -4,7 +4,7 @@
 function bubbleSort(arr) {
     let less = 0
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < (arr.length - i - 1); j++) {
+        for (let j = 0; j < (arr.length - 1); j++) {
             if (arr[j + 1] < arr[j]) {
                 less = arr[j]
                 arr[j] = arr[j + 1]
