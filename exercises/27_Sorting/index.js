01dfae73c88fc12442fe49fbc8ae63f7e6c621e9
Luiz Carneiro <luuizpaulo@gmail.com>
2019-12-13 00:36:28 +0100

    Merge sort implemented

--- exercises/27_Sorting/index.js
+++ exercises/27_Sorting/index.js
@@ -33,11 +33,31 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr
+    }
 
+    const middle = Math.floor(arr.length / 2)
+
+    const left = arr.slice(0, middle)
+    const right = arr.slice(middle)
+
+    return merge(mergeSort(left), mergeSort(right))
 }
 
 function merge(left, right) {
 
+    const results = []
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift())
+        } else {
+            results.push(right.shift())
+        }
+    }
+
+    return [...results, ...left, ...right]
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
