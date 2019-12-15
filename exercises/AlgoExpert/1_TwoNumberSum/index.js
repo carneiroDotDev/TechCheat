51957e5a24146833610d76109aee819fdeca5a0e
Luiz Carneiro <luuizpaulo@gmail.com>
2019-12-15 13:53:27 +0100

    Started the AlgoExpert files

---
+++ exercises/AlgoExpert/1_TwoNumberSum/index.js
@@ -0,0 +1,21 @@
+
+function twoNumberSum() { }
+
+// Not so performant:
+//
+// function twoNumberSum(array, targetSum) {
+//   // Write your code here.
+// 	const answer = []
+// 	for(let i =0; i<array.length; i++){
+// 		for(let j=i+1; j<array.length; j++){
+// 			if(array[i]+array[j] === targetSum){
+// 				answer.push(array[i],array[j])
+// 			}
+// 		}
+// 	}
+// 	return answer
+// }
+
+// Do not edit the line below.
+
+exports.twoNumberSum = twoNumberSum;
