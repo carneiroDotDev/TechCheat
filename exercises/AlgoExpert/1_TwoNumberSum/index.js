5edce1bb8232dad9b208ccbdd79c364a733e1416
Luiz Carneiro <luuizpaulo@gmail.com>
2019-12-21 15:18:45 +0100

    Third TwoNumberSum solution added

--- exercises/AlgoExpert/1_TwoNumberSum/index.js
+++ exercises/AlgoExpert/1_TwoNumberSum/index.js
@@ -1,20 +1,40 @@
 
-// Optimal solution O(n), but O(n) because we are 
-// using a hashtable (js object)
+// Solution with O(nlog n) due to array.sort() and 
+// O(1) because we are only using array data structure
 function twoNumberSum(array, targetSum) {
-  // Write your code here.
-  const obj = {}
+  array = array.sort(function (a, b) { return a - b })
+  let left = 0
+  let right = array.length - 1
+  let currentSum = 0
 
-  for (let num of array) {
-    if (obj[targetSum - num]) {
-      return [(targetSum - num), num]
-    } else {
-      obj[num] = true
+  while (left < right) {
+    currentSum = array[left] + array[right]
+    if (currentSum === targetSum) {
+      return [array[left], array[right]]
+    } else if (currentSum < targetSum) {
+      left += 1
+    } else if (currentSum > targetSum) {
+      right -= 1
     }
   }
-
   return []
 }
+
+// Optimal solution O(n), but O(n) because we are 
+// using a hashtable (js object)
+// function twoNumberSum(array, targetSum) {
+//   const obj = {}
+
+//   for (let num of array) {
+//     if (obj[targetSum - num]) {
+//       return [(targetSum - num), num]
+//     } else {
+//       obj[num] = true
+//     }
+//   }
+
+//   return []
+// }
 
 twoNumberSum([3, 4, -4, 8, 11, 1, -1, -6], 10) // [ 11, -1 ]
 
