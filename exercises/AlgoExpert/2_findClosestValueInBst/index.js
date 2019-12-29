ead50cedb0f904ac8735a0242c811b833f7ee581
Luiz Carneiro <luuizpaulo@gmail.com>
2019-12-29 20:41:28 +0100

    started find closest value in bst

---
+++ exercises/AlgoExpert/2_findClosestValueInBst/index.js
@@ -0,0 +1,25 @@
+function findClosestValueInBst(tree, target, diff = undefined) {
+  // Write your code here.
+  if (tree.value === target) {
+    return tree.value
+  }
+
+  if (diff) {
+    if (diff < Math.abs(tree.value - target)) {
+      diff = Math.abs(tree.value - target)
+    } else {
+      return tree.value
+    }
+  } else {
+    diff = Math.abs(tree.value - target)
+  }
+  console.log('tree -', tree)
+
+  if (tree.value < target) {
+    findClosestValueInBst(tree.right, target, diff)
+  }
+
+  if (tree.value > target) {
+    findClosestValueInBst(tree.left, target, diff)
+  }
+}
