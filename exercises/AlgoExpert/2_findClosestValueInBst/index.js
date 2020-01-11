7ed261f7732b0ebfa4c0b5639291d575f3942935
Luiz Carneiro <luuizpaulo@gmail.com>
2020-01-11 16:18:25 +0100

    Comments

--- exercises/AlgoExpert/2_findClosestValueInBst/index.js
+++ exercises/AlgoExpert/2_findClosestValueInBst/index.js
@@ -1,19 +1,21 @@
 function findClosestValueInBst(tree, target, savedNode = undefined) {
-	  if(tree.value === target){
-		  return tree.value
-	  }
-	  
-	  if(!savedNode || Math.abs(savedNode - target) > Math.abs(tree.value - target)){
-		  savedNode = tree.value
-	  }
-	  
-	  if(tree.value > target){
-			  if(!tree.left) return savedNode
-			  return findClosestValueInBst(tree.left, target, savedNode)
-		  } else {
-			  if(!tree.right) return savedNode
-			  return findClosestValueInBst(tree.right, target, savedNode)
-	  } 
+  // Write your code here.
+  if (tree.value === target) {
+    return tree.value
   }
-  
-  exports.findClosestValueInBst = findClosestValueInBst;
+
+  if (!savedNode || Math.abs(savedNode - target) > Math.abs(tree.value - target)) {
+    savedNode = tree.value
+  }
+
+  if (tree.value > target) {
+    if (!tree.left) return savedNode
+    return findClosestValueInBst(tree.left, target, savedNode)
+  } else {
+    if (!tree.right) return savedNode
+    return findClosestValueInBst(tree.right, target, savedNode)
+  }
+}
+
+// Do not edit the line below.
+exports.findClosestValueInBst = findClosestValueInBst;
