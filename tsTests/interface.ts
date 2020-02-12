70275964695d30ecbfe244e768b7ded97e981803
Luiz Carneiro <luiz.carneiro@sport1.de>
2020-02-12 13:14:00 +0100

    Added dtslint

--- tsTests/interface.ts
+++ tsTests/interface.ts
@@ -2,10 +2,16 @@ interface test1 {
     shouldWork: () => void;
     name: string
 }
+interface test2 {
+    (): void;
+}
 
+const boo: test2 = () => { console.log('works')}
 const obj1: test1 = {
     name: 'Luiz',
     shouldWork: () => { console.log(this) }
 }
 
 obj1.shouldWork.call(obj1)
+
+boo()
