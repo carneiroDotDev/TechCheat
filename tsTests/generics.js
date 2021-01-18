3cb7ea471290d4888bbf3ee5f427a0e1b0aae3c3
Luiz Carneiro <luuizpaulo@gmail.com>
2021-01-18 09:31:57 +0100

    Generics study

--- tsTests/generics.js
+++ tsTests/generics.js
@@ -1,6 +1,44 @@
-var obj = { variable: true, "var": '' };
-var stringFilter = function (val) { return typeof val === 'number'; };
+var obj = { variable: true, "var": "" };
+var saying = {
+    sayIt: function (arg1) { return arg1; }
+};
+console.log(saying.sayIt(1122));
+var stringFilter = function (val) { return typeof val === "number"; };
 stringFilter(0);
 function test() {
-    return function ok() { return ''; };
+    return function ok() {
+        return "";
+    };
 }
+// Generic extended
+function arrayToDict(array) {
+    var out = {};
+    array.forEach(function (val) {
+        out[val.id] = val;
+    });
+    return out;
+}
+var myDict = arrayToDict([
+    { id: "a", value: "first", Luiz: "Carneiro" },
+    { id: "b", value: "second", Luiz: "Carneiro" },
+]);
+// console.log(myDict);
+function startTuple(a) {
+    return function finishTuple(b) {
+        return [a, b];
+    };
+}
+var myTuple = startTuple(["first"])(33);
+function mapDict(obj, fn) {
+    var returnObj = {};
+    Object.keys(obj).forEach(function (key) {
+        var thisItem = obj[key];
+        if (typeof thisItem !== undefined) { //really nice tip when you still want values like '' or 0 to pass through
+            returnObj[key] = fn(thisItem);
+        }
+    });
+    return returnObj;
+}
+var result = mapDict({ name: 'Luiz', secondName: 'Paulo' }, function (arg) { return "*." + arg; });
+var result2 = mapDict({ age: 30 }, function (arg) { return arg + 1; });
+console.log(result2);
--- tsTests/generics.ts
+++ tsTests/generics.ts
@@ -1,22 +1,82 @@
-interface Wrap<X, Y>{
-    [ key: string ]: Y | X
+interface Wrap<X, Y> {
+  [key: string]: Y | X;
 }
 
-let obj: Wrap<string, boolean> = { variable: true, var: '' }
+let obj: Wrap<string, boolean> = { variable: true, var: "" };
 
-interface FilterFunction<T = string>{
-    (val: T): boolean;
+interface FilterFunction<Generic = string> {
+  (val: Generic): boolean;
+}
+interface sayTheName<Generic = string> { //Generic default type
+  sayIt: (arg: Generic) => Generic;
 }
 
-const stringFilter: FilterFunction<number> = (val) => typeof val === 'number'
+const saying: sayTheName<number> = {
+  sayIt: (arg1) => arg1,
+};
 
-stringFilter(0)
+// console.log(saying.sayIt(1122));
+
+const stringFilter: FilterFunction<number> = (val) => typeof val === "number";
+
+stringFilter(0);
 // stringFilter('abc')
 
-interface Fun{
-    (name: string): string
+interface Fun {
+  (name: string): string;
 }
 
-function test (): Fun{
-    return function ok(){ return '' }
+function test(): Fun {
+  return function ok() {
+    return "";
+  };
 }
+// Generic extended
+function arrayToDict<T extends { id: string }>(array: T[]): { [k: string]: T } {
+  const out: { [k: string]: T } = {};
+  array.forEach((val) => {
+    out[val.id] = val;
+  });
+  return out;
+}
+
+const myDict = arrayToDict([
+  { id: "a", value: "first", Luiz: "Carneiro" }, // must have the prop ID
+  { id: "b", value: "second", Luiz: "Carneiro" },
+]);
+
+// console.log(myDict);
+
+function startTuple<T>(a: T) { //Closure is also supported with Generics
+  return function finishTuple<U>(b: U) {
+    return [a, b] as [T, U];
+  };
+}
+
+const myTuple = startTuple(["first"])(33);
+
+// console.log(myTuple);
+
+declare function something<S extends { sayIt(): void }>(arg: S[]): void;
+declare function something(arg: { sayIt(): void }[]): void;
+
+// Exercise Dict<T> - create a map funciton for dictionaries.
+
+type dict<T> = {
+  [key: string]: T | undefined;
+};
+
+function mapDict<T, S>(obj: dict<T>, fn: (val: T) => S): dict<S> {
+  const returnObj: dict<S> = {};
+  Object.keys(obj).forEach((key) => {
+    const thisItem = obj[key];
+    if (typeof thisItem !== undefined) { //really nice tip when you still want values like '' or 0 to pass through
+      returnObj[key] = fn(thisItem);
+    }
+  });
+  return returnObj;
+}
+
+const result = mapDict({name: 'Luiz', secondName: 'Paulo'}, (arg) => `*.${arg}`)
+const result2 = mapDict({age: 30}, (arg) => arg + 1)
+console.log(result2);
