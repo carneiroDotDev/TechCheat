0abb0e55689c492d6eaa682448df78337d2fc494
Luiz Carneiro <luuizpaulo@gmail.com>
2021-02-28 20:16:13 +0100

    Added eslint and some scripts

--- effectiveTs/Item3/Item3.js
+++
@@ -1,5 +0,0 @@
-//Item 3: Understand that code generation is independent of Types
-//Part 1: Code with errors can produce output
-var x = "Hello";
-x = 123;
-console.log(x);
--- effectiveTs/Item4/Item4.ts
+++ effectiveTs/Item4/Item4.ts
@@ -1,1 +1,42 @@
 // Item 4: Confortable with structured Types
+
+interface verctor2D {
+    x: number
+    y: number
+}
+
+function calculatedLength(v: verctor2D) {
+    return Math.sqrt(v.x * v.y + v.y * v.y)
+}
+
+interface NamedVector extends verctor2D {
+    name: string
+}
+
+const v: NamedVector = { name: 'Boo', x: 3, y: 4 }
+calculatedLength(v) // TS allows calculatedLength to be called with a NamedVector
+// because its structur is compativle with vector2D.
+
+// But of course this can become a problem:
+
+interface Vector3D {
+    x: number
+    y: number
+    z: number
+}
+
+function normalize(v: Vector3D) {
+    const length = calculatedLength(v) // Z will be simply ignored, and TS allows that.
+    return {
+        x: v.x / length,
+        y: v.y / length,
+        z: v.z / length,
+    }
+}
+
+let foo: any
+foo = '1' as any
+
+console.log(foo)
+
+export {}
